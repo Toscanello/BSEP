2778bd94a5f789a8accccd6a48f7e3c22110e144
marko16 <marko.suljak80@gail.com>
2022-04-18 17:36:23 +0200

    fixed merge conflicts

--- smart-home/src/main/java/com/adminapp/domain/Certificate.java
+++ smart-home/src/main/java/com/adminapp/domain/Certificate.java
@@ -1,10 +1,15 @@
 package com.adminapp.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.UniqueConstraint;
 import java.util.Date;
 
 @Data
@@ -14,10 +19,28 @@ public class Certificate {
     @GeneratedValue
     private Long id;
 
+    @Column(name="serial_number", unique=true)
     private Long serialNumber;
+
+    @Column(name="common_name")
     private String commonName;
+
+    @Column(name="alias")
     private String alias;
+
+    @Column(name="revoked")
     private Boolean revoked;
+
+    @Column(name="date_from")
     private Date dateFrom;
+
+    @Column(name="date_until")
     private Date dateUntil;
+
+    @Column(name="is_ca")
+    private Boolean isCA;
+
+    @ManyToOne
+    @JoinColumn(name="parent_certificate", referencedColumnName = "serial_number")
+    private Certificate parentCertificate;
 }
