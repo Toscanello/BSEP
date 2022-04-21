81a0c282110a8ce94de1d6a9ff3b41b2b5240c11
Ivan <ivan.luburic99@uns.ac.rs>
2022-04-21 11:44:48 +0200

    Added functions for validating and revoking certificates

--- smart-home-admin-front/admin-app/src/pages/ViewCertificatesPage/ViewCertificatesPage.tsx
+++ smart-home-admin-front/admin-app/src/pages/ViewCertificatesPage/ViewCertificatesPage.tsx
@@ -13,25 +13,37 @@ const ViewCertificatesPage = () => {
     });
   });
 
+  function handleClickValidate(email){
+    axios.get(`http://localhost:3000/certificates/validate/${email}`).then((res)=>{
+      alert(res.data)
+    })
+  }
+  function handleClickRevoke(serialNumber){
+    axios.put(`http://localhost:3000/certificates/revoke/${serialNumber}`).then((res)=>{
+      alert('revoked')
+    })
+  }
+
   const displayData = data.map((info, key) => {
+  
     return (
       <tr key={key}>
         <td></td>
-        <td>{info.issuerDN.commonName}</td>
+        <td>{info.subjectDN.commonName}</td>
         <td>{info.issuerDN.organization}</td>
-        <td>{info.issuerDN.organizationUnit}</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td>{info.issuerDN.organizationalUnit}</td>
+        <td>{info.subjectDN.locality}</td>
+        <td>{info.subjectDN.state}</td>
+        <td>{info.subjectDN.country}</td>
+        <td>{info.subjectDN.name.split(',')[0].substring(13)}</td>
+        <td>{info.serialNumber}</td>
+        <td>{info.notBefore.split('T')[0]}</td>
+        <td>{info.notAfter.split('T')[0]}</td>
         <td>
-          <button className="btn">Validate</button>
+          <button className="btn" onClick={()=>handleClickValidate(info.subjectDN.name.split(',')[0].substring(13))}>Validate</button>
         </td>
         <td>
-          <button className="btn">Revoke</button>
+          <button className="btn" onClick={()=>handleClickRevoke(info.serialNumber)}>Revoke</button>
         </td>
       </tr>
     );
