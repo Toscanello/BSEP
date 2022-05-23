86d688c10a7c590f0a05c0d1d86fb0372a87ee44
Ivan <ivan.luburic99@uns.ac.rs>
2022-05-23 22:44:33 +0200

    CORS bug fix

--- smart-home-admin-front/admin-app/src/reg/Regex.jsx
+++ smart-home-admin-front/admin-app/src/reg/Regex.jsx
@@ -4,4 +4,4 @@ export const validName = new RegExp(
  export const validPassword = new RegExp('^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$');
  export const validSearch = new RegExp('^[A-Za-z]+$');
  export const validUsername = new RegExp("^[A-Za-z][A-Za-z0-9_]{7,29}$")
- export const varToken = "eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJhcHAtc2VjdXJpdHkiLCJzdWIiOiJ1c2VyMTIzIiwiYXV0aG9yaXR5IjpbeyJhdXRob3JpdHkiOiJST0xFX0FETUlOIn1dLCJhdWQiOiJ3ZWIiLCJpYXQiOjE2NTMzMzY1MTJ9.W_cySe_L5tgZ2c9rFBzQWs8_r2E7uyV0X6fhpy8yWRoxZCMc1cq8sxNUl0Kl62VxUe_4ODInpp5_jIe4BcsFJQ"
+ export const varToken = "eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJhcHAtc2VjdXJpdHkiLCJzdWIiOiJhZG1pbjEyMyIsImF1dGhvcml0eSI6W3siYXV0aG9yaXR5IjoiUk9MRV9BRE1JTiJ9XSwiYXVkIjoid2ViIiwiaWF0IjoxNjUzMzM3OTk4fQ.9BBdntjfD4SQ-7WatfOT6u12GZSKKEU5_CAt45Zh1ggia2LDTWzkqfaZYvXfKsWrb-1OvFhmTxOTSaxzjqpj3w"
